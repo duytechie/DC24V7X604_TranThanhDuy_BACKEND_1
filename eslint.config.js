100959fda3c605965b2f0d3f9657deca3c2dedb0
Tom <tom@users.noreply.github.com>
2026-02-12 08:43:06 +0700

    Install eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,9 @@
 import js from "@eslint/js";
 import globals from "globals";
-import prettier from "eslint-config-prettier";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
 
-export default [
-  js.configs.recommended,
-  {
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      globals: { ...globals.node },
-    },
-  },
-  prettier,
-];
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,vue}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  pluginVue.configs["flat/essential"],
+]);
