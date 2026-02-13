8d5287ae8ae2a8a5f0c20459060d2cea51e52f4e
Tom <tom@users.noreply.github.com>
2026-02-13 18:04:19 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import contactsRoute from "./app/routes/contact.route.js";
+import ApiError from "./app/api-error.js";
 
 const app = express();
 
@@ -12,5 +13,17 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRoute);
+
+// handle 404 error
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
